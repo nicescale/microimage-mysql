0b6ef66d56e9f6944e0a75236b7420e424aa6229
shellrock <hanwoody@gmail.com>
2016-01-29 20:38:08 +0800

    build from aws

--- build.sh
+++ build.sh
@@ -8,16 +8,14 @@ export IMAGE=mysql
 export TAGS="5.5 5.5.45"
 export BASE_IMAGE=microimages/alpine
 
-docker pull $REG_URL/$BASE_IMAGE
+docker pull $BASE_IMAGE
 
-docker tag -f $REG_URL/$BASE_IMAGE $BASE_IMAGE
-
-docker build -t $REG_URL/microimages/$IMAGE .
+docker build -t microimages/$IMAGE .
 
 #./test.sh
 
 for t in $TAGS; do
-  docker tag -f $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$t
+  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
 done
 
-docker push $REG_URL/microimages/$IMAGE
+docker push microimages/$IMAGE
