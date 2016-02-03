3add01259ce9ea6c8a0af520b6131e9b9cbad808
shellrock <hanwoody@gmail.com>
2016-02-03 15:27:15 +0800

    fix test

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 PASSWORD=rews8Ds
 docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
 docker run -d --name $CON_NAME -e MYSQL_ROOT_PASSWORD=$PASSWORD $IMAGE
 
-sleep 3
+sleep 10
 
 docker exec $CON_NAME mysql -e "show databases" -p$PASSWORD
 
